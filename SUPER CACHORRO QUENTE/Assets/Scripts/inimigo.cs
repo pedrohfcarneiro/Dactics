67dffbc204f5ac0d1c1e61ba61816b3b3862c5e5
phtriplox <phtriplox8090@gmail.com>
2017-08-14 22:07:45 -0300

    Adicionei projeto
    Adicionei um projeto do unity ao repositório do github para teste.

---
+++ SUPER CACHORRO QUENTE/Assets/Scripts/inimigo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inimigo : MonoBehaviour {
+    public GameObject Pizza;
+	void Start () {
+		Pizza pizzaCopia = Instantiate
+	}
+	
+	void Update () {
+
+	}
+}
